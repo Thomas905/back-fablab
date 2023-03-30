99305d6542a427be0916f49a57e59505b1408967
niquolic <nicolas.debruycker22@gmail.com>
2023-03-30 20:48:34 +0200

    merge branch nico

--- database/db.js
+++ database/db.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_URL,
-    dialect: 'mysql',
-    port: 8889
+    dialect: 'mysql'
 });
 
 (async () => {
